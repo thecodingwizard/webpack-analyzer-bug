1abe04e9a66f1f5d84a79da28619723820460490
Nathan Wang <nathan.r.wang@gmail.com>
2021-04-12 12:27:50 -0700

    add webpack bundle analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,12 @@ module.exports = {
   siteMetadata: {
     title: "Test Site",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
+      options: {
+        devMode: true,
+      },
+    },
+  ],
 };
